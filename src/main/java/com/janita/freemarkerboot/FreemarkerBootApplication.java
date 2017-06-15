133c0bb41cea074485791bcc757d4f98cc54ccc2
janita <zcj880902>
2017-06-15 17:20:02 +0800

    Initial commit

---
+++ src/main/java/com/janita/freemarkerboot/FreemarkerBootApplication.java
@@ -0,0 +1,12 @@
+package com.janita.freemarkerboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarkerBootApplication.class, args);
+	}
+}

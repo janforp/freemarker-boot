b55c6e780679ca1f1c5e699da002385c6b2bf86d
janita <zcj880902>
2017-06-16 13:34:24 +0800

    springboot使用freemaker模板引擎

--- src/main/java/com/janita/freemarkerboot/controller/UploadController.java
+++ src/main/java/com/janita/freemarkerboot/controller/UploadController.java
@@ -1,6 +1,5 @@
 package com.janita.freemarkerboot.controller;
 
-import com.janita.freemarkerboot.bean.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/janita/freemarkerboot/controller/pub/IndexController.java
+++ src/main/java/com/janita/freemarkerboot/controller/pub/IndexController.java
@@ -1,4 +1,4 @@
-package com.janita.freemarkerboot.controller;
+package com.janita.freemarkerboot.controller.pub;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
